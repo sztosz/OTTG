049fd9ca48975d62f8e4ed94191b3c904dc980ff
Bartosz Nowak <sztosz@gmail.com>
2015-10-09 00:11:16 +0200

    Added new List model and tests

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    to_do_list = models.ForeignKey(List, default=None)
